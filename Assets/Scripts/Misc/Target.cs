1dea805fefac1c5ce16ec28f98c56ec7f68ef855
Travis Alstrand <92395688+TravisAlstrand@users.noreply.github.com>
2025-01-09 12:33:04 -0800

    completed 3rd training and break

--- Assets/Scripts/Misc/Target.cs
+++ Assets/Scripts/Misc/Target.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Target : MonoBehaviour
@@ -5,11 +6,14 @@ public class Target : MonoBehaviour
     [SerializeField] private GameObject[] redBricks;
     [SerializeField] private Vector2 brickForceMin;
     [SerializeField] private Vector2 brickForceMax;
+    [SerializeField] private float timeBeforeDestroying = 2.25f;
     private bool hasBeenHit = false;
 
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.CompareTag("Kunai") && !hasBeenHit) {
             hasBeenHit = true;
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.color = Color.magenta;
             foreach (GameObject brick in redBricks)
             {
                 Rigidbody2D brickRB = brick.GetComponent<Rigidbody2D>();
@@ -17,6 +21,14 @@ public class Target : MonoBehaviour
                 brickRB.bodyType = RigidbodyType2D.Dynamic;
                 brickRB.AddForce(brickForce);
             }
+            StartCoroutine(WaitToDestroyBricks());
+        }
+    }
+
+    private IEnumerator WaitToDestroyBricks() {
+        yield return new WaitForSeconds(timeBeforeDestroying);
+        foreach (GameObject brick in redBricks) {
+            Destroy(brick);
         }
     }
 }
